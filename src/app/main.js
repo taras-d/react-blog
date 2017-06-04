e569422d94b404444cc9a554c1228cb718888734
taras-d <taras.datsenko@gmail.com>
2017-06-04 21:35:01 +0300

    Restructure styles folder

--- src/app/main.js
+++ src/app/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 
-import '../styles/styles.less';
+import '../styles/main.less';
 
 import Root from './root';
 
