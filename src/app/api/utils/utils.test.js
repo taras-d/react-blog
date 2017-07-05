bfb28e595d165b9902e7017eebacc52f0d013d1e
taras-d <taras.datsenko@gmail.com>
2017-07-05 15:12:23 +0300

    Utils tests

--- src/app/api/utils/utils.test.js
+++ src/app/api/utils/utils.test.js
@@ -2,8 +2,52 @@ import * as utils from '.';
 
 import { Observable } from 'rxjs/Observable';
 
-test('Unsubscribe subscription(s)', function() {
-    const sub = new Observable(() => {}).subscribe();
-    utils.unsub(sub);
-    expect(sub.closed).toBeTruthy();
+describe('utils', () => {
+
+    describe('unsub', () => {
+        test('subscription has to be closed', () => {
+            const sub = new Observable(() => {}).subscribe();
+            utils.unsub(sub);
+            expect(sub.closed).toBeTruthy();
+        });
+    });
+
+    describe('mimicHttpRequest', () => {
+
+        test('should return observable', function() {
+            expect( utils.mimicHttpRequest() ).toBeInstanceOf(Observable);
+        });
+
+        test('should emit data as response', done => {
+            utils.mimicHttpRequest({ 
+                delay: 100, 
+                data: { x: 1 }
+            }).subscribe(res => {
+                try {
+                    expect(res).toEqual({ x: 1 });
+                    done();
+                } catch (e) {
+                    done.fail();
+                }
+            });
+        });
+
+        test('should emit status and status text as error if processError=false', done => {
+            utils.mimicHttpRequest({ 
+                delay: 100,
+                status: 404,
+                statusText: 'Not Found',
+                processError: false
+            }).subscribe(() => {}, err => {
+                try {
+                    expect(err).toEqual({ status: 404, statusText: 'Not Found' })
+                    done();
+                } catch(e) {
+                    done.fail();
+                }
+            });
+        });
+
+    });
+
 });
